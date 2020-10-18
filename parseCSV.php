1f5e1007a8f4de866552732ddfee888b24025681
Yehuda Eisenberg <32451776+YehudaEi@users.noreply.github.com>
2020-10-18 10:22:54 +0300

    Fix two titles with the same name

--- parseCSV.php
+++ parseCSV.php
@@ -2,11 +2,11 @@
 
 // Created by Yehuda Eisenberg
 
-function parseCsv($filename, $header = true){
+function parseCsv($filename, $title = true){
     $dataText = file_get_contents($filename);
     $dataArr = explode("\n", $dataText);
 
-    if($header){
+    if($title){
         $names = explode(",", $dataArr[0]);
         unset($dataArr[0]);
     }
@@ -16,8 +16,16 @@ function parseCsv($filename, $header = true){
         $data = explode(",", $data);
         $tmp = array();
         foreach($data as $i => $dat){
-            if($header)
-                $tmp[trim($names[$i])] = trim($dat);
+            if($title){
+                if(isset($tmp[trim($names[$i])])){
+                    if(is_string($tmp[trim($names[$i])]))
+                        $tmp[trim($names[$i])] = array($tmp[trim($names[$i])], trim($dat));
+                    else
+                        $tmp[trim($names[$i])][] = trim($dat);
+                }
+                else
+                    $tmp[trim($names[$i])] = trim($dat);
+            }
             else
                 $tmp[$i] = trim($dat);
         }
